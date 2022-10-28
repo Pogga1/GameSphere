4f67f4b2825fe0d18a81bd4df18d2b63a157280b
Pogga1 <Andreweysoldt@gmail.com>
2022-10-28 13:12:29 -0700

    added final logic to edit post.js

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,4 +1,15 @@
-if (post_title && post_body && genre) {const response = await fetch (`/api/post/${postID}`, {
+
+
+const editPostForm = async event => {
+event.preventDefault();
+const title = document.querySelector('input[name="title"]').value.trim();
+const content = document.querySelector('input[name="content"]').value.trim();
+
+const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+ const response = await fetch (`/api/post/${postID}`, {
     method: 'PUT',
     body: JSON.stringify({
         post_id,
@@ -9,10 +20,27 @@ if (post_title && post_body && genre) {const response = await fetch (`/api/post/
     headers: {
         'Content-Type': 'application/json'
     },
-})
+});
+
+ if (response.ok){
+    document.location.replace('/')
+ }else{
+    alert(response.statusText)
+ }
 }
 
-if (post_title && post_body && genre) {
+document.querySelector('#edit-post')
+.addEventListener('submit', editPostForm);
+
+
+
+const deletePost = async event =>{
+    event.preventDefault();
+
+    const postId = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1
+      ];
+
 const response = await fetch (`/api/post/${postID}`, {
     method: 'DELETE',
     body: JSON.stringify({
@@ -25,5 +53,13 @@ const response = await fetch (`/api/post/${postID}`, {
         'Content-Type':
         'application/json'
     },
-})
+});
+
+if (response.ok){
+    document.location.reload('/')
+}else {
+    alert(response.statusText)
 }
+};
+document.querySelector('#delete-post')
+.addEventListener('click', deletePost)
