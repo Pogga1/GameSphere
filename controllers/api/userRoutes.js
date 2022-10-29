9865cecc2dfa767921208f4917ce1189a7e5392f
dorntrevor7 <dorntrevor7@gmail.com>
2022-10-29 12:21:57 -0700

    login route merge

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -30,7 +30,9 @@ router.post("/login", async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.json({ user: userData, message: "You are now logged in!" });
+      res
+        .status(200)
+        .json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
     res.status(400).json(err);
