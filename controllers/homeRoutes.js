f1ebeac1dd41d85b7c20763cb8ec6574860889c2
ColeS82 <25037768+ColeS82@users.noreply.github.com>
2022-10-31 21:55:22 -0700

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,7 +51,7 @@ router.get("/post/:id", async (req, res) => {
 
 router.get("/create-post", async (req, res) => {
   try {
-    const userData = await User.findByPk(1, {
+    const userData = await User.findByPk(req.user.id, {
       include: [
         { model: User, attributes: ["username"] },
         { model: Comment, include: [Post] },
